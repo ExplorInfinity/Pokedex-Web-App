26efe4aa5a7a09d6f051258fe007c800ff23031e
Aditya Singla <exploringhobby@gmail.com>
2026-03-25 00:25:40 +0530

    Implemented searching suggestion selection and calling API for data request.

--- src/search.js
+++ src/search.js
@@ -1,8 +1,26 @@
-import { PokemonAPI, getPokemon } from './api.js'
-import { updateSearchList } from './searchList.js'
+import {getPokemon, PokemonAPI} from './api.js'
+import {updateSearchList, updateCurrentInputPreview} from './searchList.js'
 
-const debouncingTime = 600;
+const debouncingTime = 500;
 const searchBar = document.getElementById('searchBarInput');
+
+async function search(input) {
+    if (!PokemonAPI.cache.nameList.find(name => input === name)) {
+        alert("No Pokemon found.");
+        return;
+    }
+
+    return await getPokemon(input);
+}
+
+function handleSearchBarInput() {
+    if(searchBar.value.trim().length === 0) {
+        updateSearchList([]);
+        return;
+    }
+    const matches = getSearchMatches(searchBar.value);
+    updateSearchList(matches, searchBar.value.trim());
+}
 
 function getSearchMatches(input) {
     input = input.trim().toLowerCase();
@@ -12,11 +30,8 @@ function getSearchMatches(input) {
 let searchTimer = null;
 searchBar.addEventListener('input', () => {
     clearTimeout(searchTimer);
-
-    searchTimer = setTimeout(async () => {
-        if(searchBar.value.trim()) {
-            const matches = getSearchMatches(searchBar.value);
-            updateSearchList(matches);
-        }
-    }, debouncingTime);
+    updateCurrentInputPreview(searchBar.value.trim());
+    searchTimer = setTimeout(handleSearchBarInput, debouncingTime);
 });
+
+export { getSearchMatches, handleSearchBarInput, search };
--- src/searchList.js
+++ src/searchList.js
@@ -1,20 +1,81 @@
+import { getSearchMatches, handleSearchBarInput, search } from "./search.js";
+
+const searchBar = document.getElementById('searchBarInput');
 const searchSuggestionsList = document.getElementById('searchSuggestions');
 
-function updateSearchList(searchList) {
-    searchSuggestionsList.innerHTML = '';
+const MaxSuggestionCount = 5;
 
-    if (searchList.length === 0) {
+let currListIndex = -1;
+searchBar.addEventListener('keydown', async (e) => {
+    if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+        e.preventDefault();
+        if (currListIndex === -1) return;
+
+        const listItems = document.querySelectorAll("#searchSuggestions li");
+
+        // Removing highlight from previously selected element
+        listItems[currListIndex].classList.remove("highlighted");
+
+        // New selection
+        const jump = Number(e.key === "ArrowDown") * 2 - 1;
+        currListIndex = (currListIndex + jump + listItems.length) % listItems.length;
+        listItems[currListIndex].classList.add("highlighted");
+        searchBar.value = listItems[currListIndex].getAttribute("value");
+    }
+
+    if (e.key === "Enter") {
+        if(currListIndex === -1) return;
+
+        const listItems = document.querySelectorAll("#searchSuggestions li");
+        searchSuggestionsList.innerHTML = '';
+        console.log(await search(listItems[currListIndex].getAttribute("value")));
+    }
+});
+
+function updateCurrentInputPreview(currentInput) {
+    if (!currentInput) return;
+
+    const listItems = document.querySelectorAll("#searchSuggestions li");
+    let currListItem;
+    if (listItems.length === 0) {
+        currListItem = appendListElement(currentInput);
+    } else currListItem = listItems[0];
+
+    searchSuggestionsList.style.display = 'block';
+    currListIndex = 0;
+    currListItem.textContent = currentInput;
+    currListItem.classList.add("highlighted");
+}
+
+function appendListElement(text) {
+    const listItem = document.createElement('li');
+    listItem.textContent = text;
+    listItem.setAttribute('value', text);
+    searchSuggestionsList.appendChild(listItem);
+    return listItem;
+}
+
+function clearList(clearInputPreview = false) {
+    const listItems = document.querySelectorAll("#searchSuggestions li");
+    if (listItems.length === 0) return;
+    for (let i = (clearInputPreview ? 0 : 1); i < listItems.length; i++) {
+        listItems[i].remove();
+    }
+}
+
+function updateSearchList(searchList, currInput = "") {
+    currListIndex = -1;
+    clearList();
+
+    if (searchList.length === 0 && currInput === "") {
         searchSuggestionsList.style.display = 'none';
         return;
     }
 
-    searchSuggestionsList.style.display = 'block';
-    const size = Math.min(5, searchList.length);
-    for (let i = 0; i < size; i++) {
-        const listItem = document.createElement('li');
-        listItem.textContent = searchList[i];
-        searchSuggestionsList.appendChild(listItem);
-    }
+    updateCurrentInputPreview(currInput);
+    const size = Math.min(MaxSuggestionCount, searchList.length);
+    for (let i = 0; i < size; i++)
+        appendListElement(searchList[i]);
 }
 
-export { updateSearchList };
+export { updateSearchList, updateCurrentInputPreview };
