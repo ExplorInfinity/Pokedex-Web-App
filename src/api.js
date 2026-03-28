a5cdd63830c2bb712a8f09ebb65d2c8cb74b106d
Aditya Singla <exploringhobby@gmail.com>
2026-03-28 22:29:21 +0530

    Minor Changes

--- src/api.js
+++ src/api.js
@@ -1,45 +1,6 @@
 import fetchURL from './fetch.js';
-import {formatString} from "./utils.js";
-
-// noinspection JSUnresolvedReference
-class Pokemon {
-    static createPokemonInstance(pokemonDetails) {
-        const pokemon = new Pokemon();
-        pokemon.id = pokemonDetails.id;
-        pokemon.name = formatString(pokemonDetails.name);
-        pokemon.lowerCaseName = pokemon.name.toLowerCase();
-        pokemon.baseExp = pokemonDetails.base_experience;
-        pokemon.height = pokemonDetails.height;
-        pokemon.weight = pokemonDetails.weight;
-        pokemon.ability = formatString(pokemonDetails.abilities.length ? pokemonDetails.abilities[0].ability.name : "None");
-        pokemon.types = pokemonDetails.types.map(t => t.type.name);
-        pokemon.image = pokemonDetails.sprites.other["official-artwork"].front_default;
-        pokemon.dreamWorldImg = pokemonDetails.sprites.other.dream_world.front_default;
-        pokemon.gif = pokemonDetails.sprites.other.showdown.front_default;
-        pokemon.descriptions = pokemonDetails.flavor_text_entries.filter(e => e.language.name === "en").map(e => e.flavor_text);
-        pokemon.category = pokemonDetails.genera.find(g => g.language.name === "en").genus;
-        pokemon.evolutionChain = pokemonDetails.evolution_chain;
-
-        pokemon.stats = {};
-        pokemonDetails.stats.forEach(s => pokemon.stats[s.stat.name] = s.base_stat );
-
-        return pokemon;
-    }
-
-    getID() {
-        return `#${this.id.toString().padStart(4, '0')}`;
-    }
-
-    getDescription() {
-        return this.descriptions[Math.floor(Math.random() * this.descriptions.length)]
-            .replace(/\f/g, " ")
-            .replace(/\n/g, " ");
-    }
-
-    getEvolutionChain() {
-
-    }
-}
+import { formatString } from "./utils.js";
+import Pokemon from "./pokemon.js";
 
 class PokemonAPI {
 
